601d08931a84cfd705637d6b6bf74febd85c2888
tokaisagami <tokai.sagami.54@gmail.com>
2024-11-28 23:13:55 +0900

    fix:Linkコンポーネント適用

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Link from 'next/link'; // 修正
 import './HomePage.css'; // CSSファイルをインポート
 
 const HomePage: React.FC = () => {
@@ -9,8 +9,12 @@ const HomePage: React.FC = () => {
         <h1 className="home-title font-KaiseiOpti">ようこそ Nightmare Appへ</h1>
         <p className="home-description">悪夢を改変し、すっきりした気分になりましょう！</p>
         <div className="home-buttons">
-          <Link to="/login" className="login-btn">ログイン</Link>
-          <Link to="/signup" className="signup-btn">新規登録</Link>
+          <Link href="/login">
+            <a className="login-btn">ログイン</a>
+          </Link>
+          <Link href="/signup">
+            <a className="signup-btn">新規登録</a>
+          </Link>
         </div>
       </div>
     </div>
