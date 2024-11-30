f72429697871f65e42d9395f7bfc937a1001fa85
tokaisagami <tokai.sagami.54@gmail.com>
2024-11-30 15:21:46 +0900

    fix:componentsをapp配下に移動

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '../../store/store';
-import { logout } from '../../store/slices/authSlice';
+import { RootState } from '../../../store/store';
+import { logout } from '../../../store/slices/authSlice';
 import { useRouter } from 'next/router';
 import Image from 'next/image'; // 追加
-import logo from '../../assets/Title-Logo2.png';
 
 const Header = () => {
   const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
@@ -19,7 +18,7 @@ const Header = () => {
 
   return (
     <header className="bg-title text-white p-4 flex justify-between items-center fixed top-0 left-0 w-full z-50">
-      <Image src={logo} alt="Nightmare App" className="h-8 sm:h-10 md:h-12 w-auto" />
+      <Image src="/images/Title-Logo2.png" alt="Nightmare App" className="h-8 sm:h-10 md:h-12 w-auto" />
       {isLoggedIn ? (
         <nav>
           <ul className="flex space-x-4">
