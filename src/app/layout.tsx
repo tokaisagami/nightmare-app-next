d09a23aa151e5c3cc0a41c6c247c9478fd3b9bab
tokaisagami <tokai.sagami.54@gmail.com>
2024-11-30 18:13:58 +0900

    add:_not-found.tsx追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#000000" /> {/* メタタグとして設定 */}
         <meta name="description" content="Web site created using Next.js" />
         <link rel="apple-touch-icon" href="/logo192.png" />
         <link rel="manifest" href="/manifest.json" />
