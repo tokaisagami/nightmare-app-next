70310492c784a220f0f9198e15bba2a8f41aec30
tokaisagami <tokai.sagami.54@gmail.com>
2024-11-30 17:22:03 +0900

    fix:_document.tsxファイルを作成し、カスタムフォントの追加部分を追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,18 +63,6 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Aoboshi+One&family=Dela+Gothic+One&family=Noto+Sans+JP&family=Noto+Sans+Linear+A&family=Noto+Serif+JP:wght@500;700;900&family=Open+Sans&family=Sawarabi+Gothic&family=Shizuru&family=Zen+Kurenaido&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Aoboshi+One&family=Dela+Gothic+One&family=Kablammo&family=Kaisei+Opti&family=Klee+One&family=Noto+Sans+JP&family=Noto+Sans+Linear+A&family=Noto+Serif+JP:wght@500;700;900&family=Open+Sans&family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&family=Sawarabi+Gothic&family=Shizuru&family=Zen+Kurenaido&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Aoboshi+One&family=Dela+Gothic+One&family=Kablammo&family=Kaisei+Decol&family=Kaisei+Opti&family=Klee+One&family=Noto+Sans+JP&family=Noto+Sans+Linear+A&family=Noto+Serif+JP:wght@500;700;900&family=Open+Sans&family=Playwrite+GB+S:ital,wght@0,100..400;1,100..400&family=Sawarabi+Gothic&family=Shizuru&family=Zen+Kurenaido&display=swap"
-          rel="stylesheet"
-        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
