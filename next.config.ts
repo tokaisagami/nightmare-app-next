0c1caa58cb15458796d26d1589f7c3aecda02f10
tokaisagami <tokai.sagami.54@gmail.com>
2024-12-01 21:26:07 +0900

    fix:クライアントとサーバー側の分離

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
